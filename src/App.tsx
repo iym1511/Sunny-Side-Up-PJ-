a20716a3b40eaf4ea515828c8ae6d44029f378dd
jinhye <sjrry112@gmail.com>
2023-04-24 17:09:03 +0900

    230424_진혜_geoapi_수정중

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SunnySideUp from './page/SunnySideUp';
 
 function App() {
   return (
     <div className="App">
       <h1>우리진혜 사랑해</h1>
+      <SunnySideUp />
     </div>
   );
 }
---
+++ src/page/SunnySideUp.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
+import { asyncFetch } from "../module/weatherApi";
+
+const SunnySideUp = () => {
+  const geoApiStatus = useAppSelector((state)=> state.geoApi.status)
+  const geoApiData = useAppSelector((state)=> {return state.geoApi.apiData});
+  const dispatch = useAppDispatch();
+
+  useEffect(()=>{
+    dispatch(asyncFetch());
+  },[])
+
+  useEffect(()=>{
+    console.log(geoApiData);
+    console.log(geoApiStatus)
+  },[geoApiStatus])
+
+  return (  
+    <div style={{border: "1px solid red"}}>
+      {
+        geoApiData &&
+        geoApiData.map((a:any)=>(
+          {a}
+        ))
+      }
+    </div>
+  );
+}
+
+export default SunnySideUp;
