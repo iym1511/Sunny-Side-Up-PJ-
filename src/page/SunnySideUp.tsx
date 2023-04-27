eb356012b4d8907fa9efd3e7c1bd6ea513adeb9a
문일윤 <mun-il-yun@mun-il-yun-ui-MacBookAir.local>
2023-04-27 13:26:14 +0900

    230427일윤 types컴포넌트화

--- src/page/SunnySideUp.tsx
+++ src/page/SunnySideUp.tsx
@@ -10,42 +10,9 @@ import { GpsState } from "../types/Gps";
 import { setGps } from "../module/gps";
 import {getAirPollData} from "../module/airpollution";
 import { getPredict30Data } from "../module/predict30";
+import { Documents, KakaoApiType } from "../types/KakaoApi";
 
-interface KakaoApiType {
-  meta: {
-    total_count: number,
-  },
-  documents: Documents[]
-};
 
-interface Documents {
-  address: {
-    address_name: string,
-    region_1depth_name: string,
-    region_2depth_name: string,
-    region_3depth_name: string,
-    mountain_yn: 'N' | 'Y',
-    main_address_no: string,
-    sub_address_no: string,
-    zip_code: string,
-    x: string,
-    y: string,
-  },
-  road_address: {
-    address_name: string,
-    region_1depth_name: string,
-    region_2depth_name: string,
-    region_3depth_name: string,
-    road_name: string,
-    underground_yn: 'N' | 'Y',
-    main_building_no: string,
-    sub_building_no: string,
-    building_name: string,
-    zone_no: string,
-    x: string,
-    y: string,
-  } | null,
-}
 
 
 
@@ -70,7 +37,7 @@ const SunnySideUp = () => {
     dispatch(asyncFetch());
     dispatch(asyncFetch2());
     dispatch(getAirPollData());
-    dispatch(getPredict30Data())
+    dispatch(getPredict30Data());
   },[]);
 
 
