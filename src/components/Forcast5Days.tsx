11e3a3484d632c8c3983c69138f640f8c982013d
문일윤 <mun-il-yun@mun-il-yun-ui-MacBookAir.local>
2023-05-06 22:19:17 +0900

    230506일윤 분류에따라 날짜출력

--- src/components/Forcast5Days.tsx
+++ src/components/Forcast5Days.tsx
@@ -1,5 +1,6 @@
 import { useAppSelector } from "../hooks/reduxHooks";
 import { List } from "../types/Predict5";
+import "../css/Forcast5Days.css"
 
 const Forcast5Days = () => {
   const predict5Data = useAppSelector((state) => {
@@ -8,51 +9,135 @@ const Forcast5Days = () => {
   const predict5Status = useAppSelector((state) => {
     return state.predict5Api.status;
   });
+
     // 시간
-    const date: Date = new Date();
-    const year: number = date.getFullYear();
-    const month: string = String(date.getMonth() + 1).padStart(2, "0");
-    const dateSelect: number = date.getDate();
-    const today: string = String(dateSelect).padStart(2, "0");
-    const useDate: string = `${month}-${today}`
-  
-    const today2: string = String(date.getDate()+1).padStart(2, "0");
-    const useDate2: string = `${month}-${today2}`
-    const today3: string = String(date.getDate()+2).padStart(2, "0");
-    const useDate3: string = `${month}-${today3}`
-    const today4: string = String(date.getDate()+3).padStart(2, "0");
-    const useDate4: string = `${month}-${today4}`
+    const today = new Date(); // 오늘 날짜 객체 생성
+    const tomorrow = new Date(today); // 오늘 날짜를 복사하여 다음 날짜 객체 생성
+    const oneDate = today.getDate(); // 현재 날짜 출력
+    const twoDate = tomorrow.getDate()+1; // 다음 날짜 출력
+    const threeDate = tomorrow.getDate()+2;
+    const fourDate = tomorrow.getDate()+3;
+    const fiveDate = tomorrow.getDate()+4;
+
   
     // 5일 일기예보 타입 가져와서 사용
-    const DateCheck:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(5, 5) == useDate ? useDate : dateSelect+1)
-    const DateCheck2:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(5, 5) == useDate2)
-    const DateCheck3:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(5, 5) == useDate3)
-    const DateCheck4:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(5, 5) == useDate4)
-  
-    const asd = () => {
-      if(predict5Data?.list.some((a: any) => a.dt_txt.substr(5, 5) == useDate)){
-        
-      }
-    }
+    const DateCheck:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(8, 2) == oneDate)
+    const DateCheck2:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(8, 2) == twoDate)
+    const DateCheck3:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(8, 2) == threeDate)
+    const DateCheck4:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(8, 2) == fourDate)
+    const DateCheck5:List[] | undefined  = predict5Data?.list.filter((a: any) => a.dt_txt.substr(8, 2) == fiveDate)
+
+
 
   return (  
     <div>
-          <div style={{display: "flex", overflowX:"scroll"}}>
-        {
-          predict5Data && predict5Data.list.map((a:any)=>(
-            <div style={{marginRight:"20px"}}>
-              <p>{a.dt_txt.substr(5, 11)}</p>
-              <p>{}</p>
-              <img
-                src={`https://openweathermap.org/img/wn/${
-                  a.weather[0].icon
-                }@2x.png`}
-                style={{width:"100%"}}
-              />
-              <p>{a.main.temp.toFixed(1)}°C</p>
-            </div>
-          ))
-        }
+        <div style={{display:"flex",overflowX:"scroll"}}>
+
+          {
+            DateCheck && DateCheck.map((data:List, index)=>(
+              <div key={index} style={{}}>
+                <span className="allSpan">오늘</span>
+                <p>{data.dt_txt.substr(5,14)}</p>
+                <img
+                    src={`https://openweathermap.org/img/wn/${
+                      data.weather[0].icon
+                    }@2x.png`}
+                    style={{width:"100%"}}
+                  />
+              </div>
+            ))
+          }
+
+          {
+            DateCheck2 && DateCheck2.map((data:List, index)=>(
+              <div key={index} style={{}}>
+                <span className="allSpan">내일</span>
+                <p>{data.dt_txt.substr(5,14)}</p>
+                <img
+                    src={`https://openweathermap.org/img/wn/${
+                      data.weather[0].icon
+                    }@2x.png`}
+                    style={{width:"100%"}}
+                  />
+              </div>
+            ))
+          }
+          
+          {
+            DateCheck3 && DateCheck3.map((data:List, index)=>(
+              <div key={index} style={{}}>
+                <span className="allSpan">모레</span>
+                <p>{data.dt_txt.substr(5,14)}</p>
+                <img
+                    src={`https://openweathermap.org/img/wn/${
+                      data.weather[0].icon
+                    }@2x.png`}
+                    style={{width:"100%"}}
+                  />
+              </div>
+            ))
+          }
+
+          {
+            DateCheck4 && DateCheck4.map((data:List, index)=>(
+              <div key={index} style={{}}>
+                <span className="allSpan">사흘</span>
+                <p>{data.dt_txt.substr(5,14)}</p>
+                <img
+                    src={`https://openweathermap.org/img/wn/${
+                      data.weather[0].icon
+                    }@2x.png`}
+                    style={{width:"100%"}}
+                  />
+              </div>
+            ))
+          }
+
+          {
+            DateCheck4 && DateCheck4.map((data:List, index)=>(
+              <div key={index} style={{}}>
+                <span className="allSpan">글피</span>
+                <p>{data.dt_txt.substr(5,14)}</p>
+                <img
+                    src={`https://openweathermap.org/img/wn/${
+                      data.weather[0].icon
+                    }@2x.png`}
+                    style={{width:"100%"}}
+                  />
+              </div>
+            ))
+          }
+
+        </div>
+
+        <div style={{display: "flex", overflowX:"scroll"}}>
+          {
+            predict5Data && predict5Data?.list.map((data:List,index) => {
+              const date:Date = new Date(data.dt_txt);
+              const year: number = date.getFullYear();
+              const month: number = date.getMonth() + 1;
+              const day: number = date.getDate();
+              const hour: number = date.getHours();
+
+              // 각 날짜별로 데이터 출력
+              if (hour === 12) {
+                return (
+                  <div key={index} style={{margin:"auto"}}>
+                    <p>{`${year}년 ${month}월 ${day}일`}</p>
+                    <img
+                    src={`https://openweathermap.org/img/wn/${
+                      data.weather[0].icon
+                    }@2x.png`}
+                    style={{width:"100%"}}
+                  />
+                    <p>{`기온: ${data.main.temp.toFixed(1)}°C`}</p>
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            })
+          }
       </div>
     </div>
   );
