0b28508631bc42fdfaf5b506097237698a208a82
문일윤 <mun-il-yun@mun-il-yun-ui-MacBookAir.local>
2023-05-16 14:41:57 +0900

    230516일윤 깃뱌포

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router";
 
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={()=>{navigate("/sunnysideup/0")}}>ㄱㄱ</button>
     </div>
   );
 };
